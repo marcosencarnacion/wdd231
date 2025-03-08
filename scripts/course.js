1a7afd32adeee1c6dc9101a4a9c64f1042a13c26
marcosencarnacion <enc24001@byui.edu>
2025-03-07 21:32:13 -0500

    Added responsive menu

--- scripts/course.js
+++ scripts/course.js
@@ -1,18 +1,34 @@
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+document.getElementById('lastModified').textContent = 'Last modified: ' + document.lastModified;
+
 const menuBtn = document.getElementById("menu-toggle");
 const menu = document.querySelector(".menu-links");
+const closeBtn = document.getElementById("close-menu");
 
 menuBtn.addEventListener("click", () => {
     menu.classList.toggle("active");
+    overlay.classList.toggle("active");
 
     if (menu.classList.contains("active")) {
         menuBtn.textContent = "✖";
     } else {
-        menuBtn.textContent = "☰"; 
+        menuBtn.textContent = "☰";
     }
 });
 
+closeBtn.addEventListener("click", () => {
+    menu.classList.remove("active");
+    overlay.classList.remove("active");
+    menuBtn.textContent = "☰";
+});
 
-document.getElementById('currentyear').textContent = new Date().getFullYear();
 
-document.getElementById('lastModified').textContent = 'Last modified: ' + document.lastModified;
+overlay.addEventListener("click", () => {
+    menu.classList.remove("active");
+    overlay.classList.remove("active");
+    menuBtn.textContent = "☰";
+});
+
+
 
